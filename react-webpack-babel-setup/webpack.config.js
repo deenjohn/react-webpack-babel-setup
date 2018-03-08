e6a614d520af4f097fbbd5ad878e26adb93dcf85
Deen John <dj.itbhu@gmail.com>
2018-03-09 01:55:13 +0530

    Update webpack.config.js

--- react-webpack-babel-setup/webpack.config.js
+++ react-webpack-babel-setup/webpack.config.js
@@ -1,43 +1,38 @@
-var webpack = require("webpack");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
 
 module.exports = {
-    entry: [
-        'react-hot-loader/patch','./src/index.js'
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/assets'
-    },
-    devServer: {
-        inline: true,
-        contentBase: './dist',
-        port: 3000,
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }, {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {
-                            minimize: true
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template: 'src/index.html'}),
-        new webpack.HotModuleReplacementPlugin()
+  entry: ["react-hot-loader/patch", "./src/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
     ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/assets",
+    filename: "bundle.js"
+  },
+  devServer: {
+    inline: true,
+    contentBase: "./dist",
+    port: 3000,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  }
 };
