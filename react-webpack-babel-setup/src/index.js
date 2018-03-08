1489ceab1bfcd4c99d9268a413b05a9b95086e2d
Deen John <dj.itbhu@gmail.com>
2018-03-09 01:27:47 +0530

    Update index.js

--- react-webpack-babel-setup/src/index.js
+++ react-webpack-babel-setup/src/index.js
@@ -1,17 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
 
 ReactDOM.render(
-				<h1
-				id='title'
-				className='header'
-				style={{
-				backgroundColor: 'orange',
-				color: 'white',
-				fontFamily: 'verdana'
-}}>
-				Hi WORLD!!
-</h1>, document.getElementById('root'));
-module
-				.hot
-				.accept();
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
